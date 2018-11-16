e7c5b57b5f0ca3618d11c98138652f998f57dd3c
maria trepakova <932400610@cit3-501m-08.acg.ac.nz>
2018-11-16 18:03:42 +1300

    Sidebar animation. Sidebar styling.

--- js/scroll.js
+++ js/scroll.js
@@ -21,7 +21,7 @@ $(function(){
 
 		}
 
-		if( iScrollTop>navOffset.top){
+		if( iScrollTop>navOffset.top - 150){
 			$('.sidebar').addClass('show');
 		}else{
 			//--unfix it
@@ -40,7 +40,45 @@ $(function(){
 			$('.home').removeClass('fixed');
 
 		}
+
+		//-----higlights the right section in the nav while scrolling
+		var offset1 = $('.section-2').offset().top - 50;
+		var offset2 = $('.section-3').offset().top - 50;
+		var offset3 = $('.section-4').offset().top - 50;
+		var offset4 = $('.section-5').offset().top - 50;
+
+		let activeLi;
+
+		if (iScrollTop>=offset1 && iScrollTop < offset2){
+			activeLi = $('.sidebar-list>li:nth-child(1)');
+		}
+		if (iScrollTop>=offset2 && iScrollTop < offset3){
+			activeLi = $('.sidebar-list>li:nth-child(2)');
+		}
+		if (iScrollTop>=offset3 && iScrollTop < offset4){
+			activeLi = $('.sidebar-list>li:nth-child(3)');
+		}
+		if (iScrollTop>=offset4){
+			activeLi = $('.sidebar-list>li:nth-child(4)');
+		}
+
+		activeLi.addClass('active');
+		$('.sidebar-list>li').not(activeLi).removeClass('active');
 	
+
+
+	
+	});
+
+	///------Navigation between sections
+	$('.sidebar-link').on('click', function(e){
+		
+		e.preventDefault();
+
+			var sTarget = $(this).attr('href');
+			var targetOffset = $(sTarget).offset().top;
+			$('html, body').animate({scrollTop:targetOffset},1000);
+
 	});
 
 });
